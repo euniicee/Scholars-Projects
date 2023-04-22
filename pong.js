2cb7ac72b0796fbf38737ac4dc1497e5c4ab1db5
Opeoluwa-Ogunlaja <ogunlajafaith@gmail.com>
2023-04-22 22:02:41 +0100

    Properties and methods

--- pong.js
+++ pong.js
@@ -1,3 +1,14 @@
 // Getting the canvas element fromm our javascript
 const canvas = document.getElementById('gameArea')
 document.body.style.backgroundColor = '#1f1f1f'
+
+const ball = {
+    color: 'green',
+    changeColor: function(){
+        // A method, part of an object:ball
+    }
+}
+
+function changeColor(color){
+    // Not a method, does not belong to an object. Just a function
+}
